5a71de8a193aae42ef72ff5a055f76de2998c787
Saugat Poudel <njsaugat@gmail.com>
2022-07-15 09:51:39 +0545

    script file added

---
+++ expenseTracker/script.js
@@ -0,0 +1,148 @@
+const itemName=document.body.querySelector('.item-name input')
+const itemPrice=document.body.querySelector('.item-price input')
+const addTransaction=document.body.querySelector('.add-tranaction')
+
+const amount=document.body.querySelector('.amount')
+const items=document.body.querySelector('.items')
+
+const incomeAmt=document.body.querySelector('.income-amt')
+const expenseAmt=document.body.querySelector('.expense-amt')
+
+let expenseTrackerItems=[]
+let income=0;
+let expense=0;
+let index=0;
+if(JSON.parse(localStorage.getItem('expenseTrackerItems'))){
+    const items=JSON.parse(localStorage.getItem('expenseTrackerItems'))
+    items.forEach(item=>{
+        createRecord(item[0],item[1],index)
+        index++
+        
+        updateIncomeExpenseCreation(+item[1])
+        updateBalance(+item[1])
+    })
+}
+
+
+
+
+addTransaction.addEventListener('click',()=>{
+    if(itemName.value && itemPrice.value && checkNumber(itemPrice.value) ){
+        createRecord(itemName.value,itemPrice.value,index)
+        index++
+        
+        updateIncomeExpenseCreation(+itemPrice.value)
+        updateBalance(+itemPrice.value)
+        itemName.value=''
+        itemPrice.value=''
+        
+    }
+})
+
+
+
+function checkNumber(money){
+    return money.match(/^-?\d+\.?\d*$/)
+}
+
+function updateIncomeExpenseCreation(money){
+    if(money>0){
+        income+=money
+        incomeAmt.innerText=`$${income}.00`
+    }else{
+        money=money*(-1);
+        expense+=money
+        expenseAmt.innerText=`$${expense}.00`
+    }
+    updateLS()
+}
+
+function updateIncomeExpenseDeletion(money){
+    if(money>0){
+        income-=money
+        incomeAmt.innerText=`$${income}.00`
+
+    }else{
+        money=money*(-1);
+        expense-=money
+        expenseAmt.innerText=`$${expense}.00`
+
+    }
+    updateLS()
+}
+
+
+function updateBalance(currBalance){
+    const dollorAmt=amount.innerText
+    const index=dollorAmt.indexOf('$')
+    let dollorInt=+ dollorAmt.slice(index+1,dollorAmt.length)
+    if(dollorAmt.includes('-')){
+        dollorInt=dollorInt*(-1)
+    }
+    const finalBalance=dollorInt+currBalance
+    if(finalBalance<0){
+        amount.innerText=`-$${finalBalance*(-1)}.00`
+    }else{
+        amount.innerText=`$${finalBalance}.00`
+    }
+}
+
+
+
+function createRecord(name,price,index){
+    if(!items.innerHTML){
+        items.innerHTML+=`      <span>Tracked:</span>
+        <div class="tracked-line"></div>
+  `
+    }
+    const overallItem=document.createElement('div')
+    overallItem.classList.add('overall')
+    overallItem.innerHTML+=` 
+        <button><i class="fa-solid fa-xmark"></i></button>
+        <div class="item">
+          <span class="item-name">${name}</span>
+          <span class="item-amt">${price}</span>
+          <!-- <span class="color-representation"></span> -->
+        </div>`
+
+    itemDeletion(overallItem,price,index)
+    changeBorder(overallItem,price)
+    items.appendChild(overallItem)
+    
+    expenseTrackerItems.push([name,price])
+    updateLS()
+}
+
+function itemDeletion(overallItem,price,index){
+    overallItem.childNodes[1].addEventListener('click',()=>{
+        console.log(index)
+        expenseTrackerItems.splice(index, 1)
+        updateLS()
+        decreaseIndex()
+        overallItem.remove()
+        updateBalance((+price)*(-1))
+        updateIncomeExpenseDeletion((+price))
+    })
+
+}
+
+
+
+function decreaseIndex(){
+    // const overallItem=document.body.querySelectorAll('.overall')
+    // if(index!==0 && index===overallItem.length-1){
+    //     index--;
+    // }
+    index--;
+}
+function changeBorder(overallItem,price){
+    if(+price>0){
+        overallItem.childNodes[3].style.borderRight=`10px solid rgb(26, 180, 12)`
+    }else if(+price<0){
+        overallItem.childNodes[3].style.borderRight=`10px solid red`
+    }
+}
+
+function updateLS(){
+    localStorage.setItem('expenseTrackerItems',JSON.stringify(expenseTrackerItems))
+}
