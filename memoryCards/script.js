dd7e8c54bdf664594d6dd2ed81ffd0a0d7040822
Saugat Poudel <njsaugat@gmail.com>
2022-07-17 07:17:39 +0545

    script file added

--- memoryCards/script.js
+++ memoryCards/script.js
@@ -1,31 +1,99 @@
-// const fields=document.body.querySelectorAll('textarea')
-
-// fields.forEach(field=>{
-//     field.addEventListener('change',()=>{
-//         field.classList.add('active')
-//     })
-// })
-
 const memoryCards=document.body.querySelectorAll('.memory-card')
-const addNewCart=document.body.querySelector('.add-new-cart')
+const addNewCard=document.body.querySelector('.add-new-card')
 const formControl=document.body.querySelector('.form-control')
-console.log(formControl)
+const crossCard=document.body.querySelector('.cross')
+const question=document.body.querySelector('.question')
+const answer=document.body.querySelector('.answer')
+
+
+const memoryCardes=document.body.querySelector('.memory-cards')
+const textareas=document.body.querySelectorAll('textarea')
+console.log(answer)
+console.log(question)
+// const fo=document.body.querySelector('.add-card')
+let obj=[]
+
 // memoryCards.forEach(memoryCard=>{
 //     memoryCard.addEventListener('click',()=>{
 //         memoryCard.classList.toggle('flipped')
-//         memoryCard.innerText=''
-//         setTimeout(()=>{
-//             memoryCard.classList.toggle('flipped')
-//         },250)
-//         // memoryCard.childNodes[5].innerText=`A programming Language`
 //     })
 // })
 
-
-addNewCart.addEventListener('click',()=>{
+addNewCard.addEventListener('click',()=>{
     console.log(formControl)
-    addNewCart.classList.add('active')
+    // addNewCard.classList.add('active')
     formControl.classList.add('active')
+   
+})
+
+crossCard.addEventListener('click',()=>{
+    // addNewCard.classList.remove('active')
+    formControl.classList.remove('active')
+    
+})
+
+formControl.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    // console.log(question.value)
+    // console.log(answer.value)
+    obj.push({question:question.value,answer:answer.value})
+    console.log(obj)
+    showCards(obj)
+    formControl.classList.remove('active')
 
 })
+
+
+function showCards(obj){
+    obj.forEach(ob=>{
+        const memoryCard=document.createElement('div')
+        memoryCard.classList.add('memory-card')
+        memoryCard.innerHTML=`
+        <span class="flip"><i class="fa-solid fa-rotate"></i>Flip</span>
+        <span class="question-card card">${ob.question}</span>
+        <span class="answer-card card inactive" >${ob.answer}</span>
+
+        `
+        memoryCard.addEventListener('click',()=>{
+            const answerCard=document.body.querySelector('.answer-card')
+            const questionCard=document.body.querySelector('.question-card')
+
+            // questionCard.innerText=''
+            // answerCard.innerText=ob.answer
+            memoryCard.classList.add('flipped')
+            setTimeout(()=>{
+                memoryCard.classList.remove('flipped')
+
+            },250)
+            
+            if(answerCard.classList.contains('inactive')){
+                answerCard.classList.remove('inactive')
+                questionCard.classList.add('inactive')
+            }else{
+                answerCard.classList.add('inactive')
+                questionCard.classList.remove('inactive')                
+            }
+            
+        })
+        memoryCardes.appendChild(memoryCard)
+    })
+}
+
+textareas.forEach(textarea=>{
+    // console.log(textarea)
+    textarea.addEventListener('change',(e)=>{
+        // console.log(e)
+        // console.log(e.target.value)
+        textarea.innerText=e.target.value
+        // passAnswer(e.target.value)
+    })
+})
+
+
+
+
+
+
+
 
