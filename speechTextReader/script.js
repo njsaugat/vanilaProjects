f7fda86be38a053fcc87056c1db2554ffe3788cd
Saugat Poudel <njsaugat@gmail.com>
2022-07-27 11:15:29 +0545

    script file added

--- speechTextReader/script.js
+++ speechTextReader/script.js
@@ -1,7 +1,12 @@
 const closeBox=document.body.querySelector('.close')
 const toggleBtn=document.body.querySelector('.btn-toggle')
 const toggleBox=document.body.querySelector('.text-box')
-console.log(toggleBox)
+const container=document.body.querySelector('.container')
+const voiceSelect=document.body.querySelector('select')
+const readText=document.body.querySelector('.read-text')
+const textarea=document.body.querySelector('textarea')
+let voices=[]
+// console.log(toggleBox)
 const activities=document.body.querySelector('.items')
 
 const activitiesList=[
@@ -72,10 +77,79 @@ function createActivity(activity,index){
 toggleBtn.addEventListener('click',()=>{
     console.log(123)
     toggleBox.classList.add('active')
+    // document.body.style.backgroundColor=`grey`
+    container.classList.add('active')
+    document.body.style.overflow=`hidden`
 })
 
 closeBox.addEventListener('click',()=>{
     toggleBox.classList.remove('active')
+    // document.body.style.backgroundColor=`white`
+    container.classList.remove('active')
+    document.body.style.overflow=`auto`
+
 })
 
+function addEventListeners(){
+    const items=document.body.querySelectorAll('.item')
+    // console.log(items)
+    items.forEach(item=>{
+        item.addEventListener('click',()=>{
+            item.classList.add('active')
+            setMessage(item.innerText)
+            speakTextMessage()
+            setTimeout(()=>{
+                item.classList.remove('active')
+            },500)
+        })
+    })
+}
 
+addEventListeners()
+
+
+const message=new SpeechSynthesisUtterance()
+
+
+// const synth = window.speechSynthesis;
+// console.log(speechSynthesis)
+function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+    // if(typeof speechSynthesis === 'undefined') {
+    //     return;
+    //   }
+    console.log(voices)
+    // for(let i = 0; i < voices.length ; i++) {
+    voices.forEach(voice=>{        
+        const option = document.createElement('option');
+        option.textContent = `${voice.name} (${voice.lang})`;
+    
+        if(voice.default) {
+          option.textContent += ' — DEFAULT';
+        }
+    
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    })
+    // }
+}
+
+
+speechSynthesis.addEventListener('voiceschanged',populateVoiceList)
+
+populateVoiceList()
+
+
+function setMessage(text){
+    message.text=text;
+}
+
+function speakTextMessage(){
+    speechSynthesis.speak(message)
+}
+
+readText.addEventListener('click',()=>{
+    setMessage(textarea.value)
+    speakTextMessage()
+})
