80b66bb1be51f318c38a5ad45215e540c1070bef
Saugat Poudel <njsaugat@gmail.com>
2022-07-30 17:35:54 +0545

    script file updated

---
+++ reactStarter/script.js
@@ -0,0 +1,3 @@
+// ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello React</h1>);
